d508f5299e7bf17c2fec45e4278b580358342571
Krishna Nallani <chaitu.nc@gmail.com>
2017-11-14 16:07:40 -0500

    test

--- src/main/java/org/test/ClientApplication.java
+++ src/main/java/org/test/ClientApplication.java
@@ -3,6 +3,8 @@ package org.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.post.LocationRewriteFilter;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,6 +13,11 @@ public class ClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 
+	}
+
+	@Bean
+	public LocationRewriteFilter locationRewriteFilter() {
+		return new LocationRewriteFilter();
 	}
 
 }
