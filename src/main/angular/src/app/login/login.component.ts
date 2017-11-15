83355e7dbe9845ccafa996d274dc85e71ca385ff
Krishna Nallani <chaitu.nc@gmail.com>
2017-11-15 11:19:12 -0500

    getuser

--- src/main/angular/src/app/login/login.component.ts
+++ src/main/angular/src/app/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent implements OnInit {
     constructor(private authSvc: AuthService) { }
 
     ngOnInit() {
-
+        this.getUser();
     }
 
     public getUser(): void {
