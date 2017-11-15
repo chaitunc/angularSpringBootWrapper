e2cb1fe1aa5d9026bdb0bff3511a7bb3329a813b
Krishna Nallani <chaitu.nc@gmail.com>
2017-11-15 13:00:27 -0500

    test

--- src/main/java/org/test/ClientApplication.java
+++ src/main/java/org/test/ClientApplication.java
@@ -3,6 +3,8 @@ package org.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -11,6 +13,14 @@ public class ClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 
+	}
+
+	@Controller
+	public class HomeController {
+		@RequestMapping("/home")
+		public String forward() {
+			return "forward:/index.html";
+		}
 	}
 
 }
